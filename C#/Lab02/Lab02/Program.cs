bae2d14871ed4af87f00995bcbf3e083bd3fcaa6
damazi1 <114773869+damazi1@users.noreply.github.com>
2024-06-03 17:12:33 +0200

    Lab02 Wskaźniki do funkcji

--- C#/Lab02/Lab02/Program.cs
+++ C#/Lab02/Lab02/Program.cs
@@ -1,152 +1,80 @@
-﻿namespace Lab02
+﻿using System;
+using System.Collections.Generic;
+
+namespace lab02
 {
-    class Odcinek : IComparable<Odcinek>
+    class OperacjeNaLiscie
     {
-        public double x1, y1, x2, y2;
-        public Odcinek(double x1, double y1, double x2, double y2)
+        public static List<T> Zmiana<T>(List<T> lista, Func<T, T> operacja)
         {
-            this.x1 = x1; this.x2 = x2; this.y1 = y1; this.y2 = y2;
-        }
-        public double Dlugosc()
-        {
-            return Math.Sqrt((x2 - x1) * (x2 - x1)+(y2 - y1)*(y2-y1));
-        }
-        public int CompareTo(Odcinek odc) {
-            if (this.Dlugosc() < odc.Dlugosc())
-                return 1;
-            else
-                return 0;
+            List<T> rezultat = new List<T>();
+            for (int i = 0; i < lista.Count; i++)
+            {
+                rezultat.Add(operacja(lista[i]));
+            }
+            return rezultat;
         }
     }
-    class Osoba : IComparable<Osoba>
+    class OperacjeNaLiscie1
     {
-        public string imie, nazwisko;
-        public int wiek;
-        public Osoba(string imie, string nazwisko,  int wiek)
+        public static List<T> Zmiana<T>(List<T> lista, char znak, Func<T,char, bool> warunek)
         {
-            this.imie = imie; this.nazwisko = nazwisko; this.wiek = wiek;
-        }
-        public int CompareTo(Osoba os)
-        {
-            if (this.wiek < os.wiek)
-                return 1;
-            else
-                return 0;
-        }
-        public override string ToString()
-        {
-            return $"{imie} {nazwisko} / wiek: {wiek}";
+            List<T> rezultat = new List<T>();
+            for (int i = 0; i < lista.Count; i++)
+            {
+                if (warunek(lista[i], znak)) rezultat.Add(lista[i]);
+            }
+            return rezultat;
         }
     }
-    class Node<T> where T : IComparable<T>
+    public class Program
     {
-        public T Value;
-        public Node<T> Left, Right;
-        public Node(T value)
-        {
-            this.Value = value;
-            this.Left = null;
-            this.Right = null;
-        }
-    }
-    class BinartSearchTree<T> where T : IComparable<T>
-    {
-        private Node<T> root;
-        public BinartSearchTree()
-        {
-            root = null;
-        }
-        public void Insert(T value)
-        {
-            root = InsertRec(root, value);
-        }
-        private Node<T> InsertRec(Node<T> root, T value)
-        {
-            if (root == null)
-            {
-                root = new Node<T>(value);
-                return root;
-            }
-            if (value.CompareTo(root.Value) < 0)
-            {
-                root.Left = InsertRec(root.Left, value);
-            }
-            else if (value.CompareTo(root.Value) > 0)
-            {
-                root.Right = InsertRec(root.Right, value);
-            }
-            return root;
-        }
-        public void InOrderTravarsal(Action<T> action)
-        {
-            InOrderRec(root,action);
-        }
-        private void InOrderRec(Node<T> root, Action<T> action) { 
-            if(root != null)
-            {
-                InOrderRec(root.Left, action);
-                action(root.Value);
-                InOrderRec(root.Right,action);
-            }
-        }
-    
-    }
-    internal class Program
-    {
-        public static void sortuj<T>(T[] tablica) where T : IComparable<T> {
-            T tmp;
-            bool zmienna = true;
-            while (zmienna)
-            {
-                zmienna = false;
-                for (int i = 0; i < tablica.Length - 1; i++)
-                {
-                    if (tablica[i].CompareTo(tablica[i + 1]) == 1)
-                    {                        
-                            zmienna = true;
-                            tmp = tablica[i];
-                            tablica[i] = tablica[i + 1];
-                            tablica[i + 1] = tmp;
-                    }
-                }
-            }
-        }
-        static void Main(string[] args)
-        {
-            Console.WriteLine("+++++ Przyklad 1 +++++");
-            Odcinek o1 = new Odcinek(0,0,1,1);
-            Odcinek o2 = new Odcinek(0,0,2,2);
-            Odcinek o3 = new Odcinek(0,0,0.5,0.5);
-            Odcinek o4 = new Odcinek(0,0,3,4);
-            Odcinek o5 = new Odcinek(0, 0, 5, 5);
+        static int PodniesDoKwadratu(int x) => x * x;
+        static bool TylkoNaP(string wartosc, char znak) => wartosc[0] == znak;
+        public static Func<T, T, T> odwroc<T>(Func<T, T, T> Funk) => (T a, T b) => Funk(b, a);
+        static Func<double, double, double> fun1 = (a, b) => a / b;
+        public static Func<double, double> zloz(Func<double, double> f1, Func<double, double> f2) => (double x) => f1(f2(x));
+        public static Func<double, double> fun3 = (double a) => a * a;
+        public static Func<double, double> fun4 = (double a) => a + a;
 
-            Odcinek[] tab = { o1, o2, o3, o4, o5 };
-            int[] tab2 = { 3, 4, -2, 5, 6 };
-            for (int i = 0; i < tab.Length; i++)
-                Console.WriteLine("dlugosc = " + tab[i].Dlugosc());
-            sortuj<Odcinek>(tab);
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("---------- Operacje na liczbach ----------");
+            List<int> liczby = new List<int>() { 1, 2, 3, 4, 5 };
+            for (int i = 0;i < liczby.Count;i++) Console.WriteLine(liczby[i]);
             Console.WriteLine("------------------------------");
-            for (int i = 0; i < tab.Length; i++)
-                Console.WriteLine("dlugosc = " + tab[i].Dlugosc());
-            sortuj<int>(tab2);
+            List<int> liczby2 = OperacjeNaLiscie.Zmiana(liczby, PodniesDoKwadratu);
+            for (int i = 0; i<liczby2.Count; i++) Console.WriteLine(liczby2[i]);
 
+            Console.WriteLine("---------- Operacje na stringach ----------");
+            List<string> lista1 = new List<string>()
+             {
+             "jakis tekst",
+             "pewien tekst",
+             "przypakowy tekst",
+             "dziwny tekst"
+             };
 
-            Console.WriteLine("+++++ Zad 1.4 +++++");
-            Osoba os1 = new Osoba("Jan", "Nowak", 15);
-            Osoba os2 = new Osoba("Hubert", "Pawlik", 59);
-            Osoba os3 = new Osoba("Krzysztof", "Kowalski", 11);
-            Osoba os4 = new Osoba("Anna", "Mala", 20);
-            Osoba[] ostab = { os1, os2, os3, os4 };
-            for (int i = 0; i < ostab.Length; i++)
-                Console.WriteLine(ostab[i].ToString());
-            sortuj<Osoba>(ostab);
-            Console.WriteLine("------------------------------");
-            for (int i = 0; i < ostab.Length; i++)
-                Console.WriteLine(ostab[i].ToString());
+            for (int i = 0; i < lista1.Count; i++)
+                Console.WriteLine(lista1[i]);
+            Console.WriteLine("-------------------");
+            List<string> lista2 =
+            OperacjeNaLiscie1.Zmiana(lista1, 'p', TylkoNaP);
+            for (int i = 0; i < lista2.Count; i++)
+                Console.WriteLine(lista2[i]);
 
-            Console.WriteLine("------------------------------");
+            Console.WriteLine("---------- Operacje na Funkcjach ----------");
+            var x1 = 10;
+            var x2 = 2;
+            var wynik1 = fun1(x1, x2);
+            var fun2 = odwroc(fun1);
+            var wynik2 = fun2(x2, x1);
+            Console.WriteLine($"Wynik 1 = {wynik1}, Wynik 2 = {wynik2}");
 
-            
+            var wynik3 = zloz(fun3, fun4);
+            double result = wynik3(5);
+            Console.WriteLine($"Wynik zlozenia = {result}");
+
         }
     }
 }
